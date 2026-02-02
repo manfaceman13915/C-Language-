9900bc1d84001d2a9323ba24fdc8307dddc9b954
manfaceman13915 <manfaceman13915@gmail.com>
2026-02-02 23:00:05 +0700

    Create Comparison Operators <=,>=.cpp

---
+++ Comparison Operators <=,>=.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  int age = 13; 
+  cout << (age >= 13) << endl;   //แสดง 1
+  cout << (age <= 13) << endl;   //แสดง 1
+}
