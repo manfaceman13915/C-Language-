d2858c8079aa652570e31b08905c8ea36bda8988
manfaceman13915 <manfaceman13915@gmail.com>
2026-02-02 22:59:01 +0700

    Create Comparison Operators !=,==.cpp

---
+++ Comparison Operators !=,==.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  int a = 10, b = 10, c = 5, d = 15, e = 20, f = 15;
+  cout << (a == b) << endl;  //แสดง 1
+  cout << (a != c) << endl;  //แสดง 1
+
+  cout << (d == e) << endl;  //แสดง 0
+  cout << (d != f) << endl;  //แสดง 0
+}
+
